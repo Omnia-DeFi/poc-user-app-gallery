fc0889883dda62628d8c6e107b1793589e6c91a8
techx1 <ss.uitech01@gmail.com>
2022-07-24 23:54:02 +0500

    feat(swipe): add see all assets again button

--- src/containers/swipe-product/index.jsx
+++ src/containers/swipe-product/index.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useRef, useMemo } from "react";
+/* eslint-disable react/jsx-no-useless-fragment */
+import React, { useState, useMemo, useEffect, useRef } from "react";
 import Product from "@components/product";
 import PropTypes from "prop-types";
 import { SectionTitleType, ProductType } from "@utils/types";
 import TinderCard from "react-tinder-card";
+import Button from "@ui/button";
 
 const SwipeProduct = ({ data, swipe }) => {
     const [currentIndex, setCurrentIndex] = useState(data.products.length - 1);
-    const [lastDirection, setLastDirection] = useState();
+    const [showCards, setShowCards] = useState(false);
     const currentIndexRef = useRef(currentIndex);
 
     const childRefs = useMemo(
@@ -22,14 +24,15 @@ const SwipeProduct = ({ data, swipe }) => {
         currentIndexRef.current = val;
     };
 
-    const swiped = (direction, nameToDelete, index) => {
-        setLastDirection(direction);
+    const swiped = (index) => {
         updateCurrentIndex(index - 1);
     };
 
-    const canGoBack = currentIndex < data.products.length - 1;
-
     const canSwipe = currentIndex >= 0;
+
+    useEffect(() => {
+        setShowCards(canSwipe);
+    }, [currentIndex]);
 
     const swipeInDirection = async (dir) => {
         if (canSwipe && currentIndex < data.products.length) {
@@ -38,42 +41,69 @@ const SwipeProduct = ({ data, swipe }) => {
     };
 
     return (
-        <div style={{ position: "relative" }}>
-            <div className="swipe-card-container">
-                {data.products.slice(0, 10).map((prod, index) => (
-                    <div className="swipe" key={prod.id}>
-                        <TinderCard
-                            onSwipe={(dir) => swiped(dir, prod, index)}
-                            ref={childRefs[index]}
-                            preventSwipe={["up", "down"]}
-                        >
-                            <div className="swipe-card">
-                                <Product
-                                    overlay
-                                    placeBid={!!data.placeBid}
-                                    title={prod.title}
-                                    slug={prod.slug}
-                                    latestBid={prod.latestBid}
-                                    price={prod.price}
-                                    likeCount={prod.likeCount}
-                                    auction_date={prod.auction_date}
-                                    image={prod.images?.[0]}
-                                    authors={prod.authors}
-                                    bitCount={prod.bitCount}
-                                    swipe={swipe}
-                                />
+        <>
+            {showCards ? (
+                <div style={{ position: "relative" }}>
+                    <div className="swipe-card-container">
+                        {data.products.map((prod, index) => (
+                            <div className="swipe" key={prod.id}>
+                                <TinderCard
+                                    onSwipe={() => swiped(index)}
+                                    ref={childRefs[index]}
+                                    preventSwipe={["up", "down"]}
+                                >
+                                    <div className="swipe-card">
+                                        <Product
+                                            overlay
+                                            placeBid={!!data.placeBid}
+                                            title={prod.title}
+                                            slug={prod.slug}
+                                            latestBid={prod.latestBid}
+                                            price={prod.price}
+                                            likeCount={prod.likeCount}
+                                            auction_date={prod.auction_date}
+                                            image={prod.images?.[0]}
+                                            authors={prod.authors}
+                                            bitCount={prod.bitCount}
+                                            swipe={swipe}
+                                        />
+                                    </div>
+                                </TinderCard>
                             </div>
-                        </TinderCard>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <i
-                onClick={() => swipeInDirection("left")}
-                className="feather-arrow-right-circle swipe-right-arrow"
-            />
+                    <i
+                        onKeyPress={() => swipeInDirection("right")}
+                        role="button"
+                        tabIndex={0}
+                        aria-label="swipe-right"
+                        onClick={() => swipeInDirection("right")}
+                        className="feather-arrow-right-circle swipe-right-arrow"
+                    />
 
-            <i className="feather-arrow-left-circle swipe-left-arrow" />
-        </div>
+                    <i
+                        onKeyPress={() => swipeInDirection("left")}
+                        role="button"
+                        tabIndex={0}
+                        aria-label="swipe-right"
+                        onClick={() => swipeInDirection("left")}
+                        className="feather-arrow-left-circle swipe-left-arrow"
+                    />
+                </div>
+            ) : (
+                <div className="products-again-container">
+                    <p>You have seen all the assets</p>
+                    <Button
+                        onClick={() => {
+                            setShowCards(true);
+                            setCurrentIndex(data.products.length - 1);
+                        }}
+                    >
+                        See assests again
+                    </Button>
+                </div>
+            )}
+        </>
     );
 };
 
