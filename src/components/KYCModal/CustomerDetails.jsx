13432acaca2f34c24264c1fd5b76d993d41fba25
Sahinur Islam <infosahinur@gmail.com>
2022-08-16 00:19:36 +0600

    [ADD] Take a selfie section

--- src/components/KYCModal/CustomerDetails.jsx
+++ src/components/KYCModal/CustomerDetails.jsx
@@ -1,96 +1,101 @@
 import React from "react";
-import { Col, FormGroup } from "react-bootstrap";
 // import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Button from "@ui/button";
 
-function CustomerDetails() {
+function CustomerDetails({ firstStepHandler }) {
     return (
-        <div>
-            <form>
-                <div className="row p-3">
-                    <label htmlFor="">Your full name</label>
-                    <input
-                        className="border p-3 rounded"
-                        type="text"
-                        required
-                        name="username"
-                        placeholder="eg: Raj Kumar Babu"
-                    />
-                    <p>Ensure it matches name on your idendity documents</p>
-                    <label htmlFor="">Your date of birth</label>
-                    <input
-                        className="border p-3 rounded"
-                        type="date"
-                        required
-                        name="dateofbirth"
-                        placeholder="DD/MM/YYYY"
-                    />
+        <>
+            <Modal.Header>
+                <div className="d-flex justify-content-center ">
+                    <div>
+                        <img
+                            style={{ width: "50px", height: "50px" }}
+                            className="me-4 mt-3 "
+                            src="/images/KYC/logo.png"
+                            alt=""
+                        />
+                    </div>
+                    <div>
+                        <h6 className="my-2">
+                            Hi ! Help us Setup your account
+                        </h6>
+                        <p className="m-0">
+                            We'll verify it with your KYC documents
+                        </p>
+                    </div>
+                </div>
+            </Modal.Header>
+            <Modal.Body>
+                <form>
+                    <div className="row p-3">
+                        <label htmlFor="">Your full name</label>
+                        <input
+                            className="border p-3 rounded"
+                            type="text"
+                            required
+                            name="username"
+                            placeholder="eg: Raj Kumar Babu"
+                        />
+                        <p>Ensure it matches name on your idendity documents</p>
+                        <label htmlFor="">Your date of birth</label>
+                        <input
+                            className="border p-3 rounded"
+                            type="date"
+                            required
+                            name="dateofbirth"
+                            placeholder="DD/MM/YYYY"
+                        />
 
-                    <div className="mt-3">
-                        <label className="text-color">Your gender</label>
-                        <div className="row mt-3">
-                            <div className="col-4">
-                                <button
-                                    type="button"
-                                    className="bbutton col-12 border btn btn-block btn-success btn-outline-success"
-                                    value="Female"
-                                    // onClick={() => this.changegender("Female")}
-                                >
-                                    Female
-                                </button>
-                            </div>
-                            <div className="col-4">
-                                <button
-                                    type="button"
-                                    className="bbutton border RoundButton btn btn-block btn-outline-success"
-                                    value="Male"
-                                    // onClick={() => this.changegender("Male")}
-                                >
-                                    Male
-                                </button>
-                            </div>
-                            <div className="col-4">
-                                <button
-                                    type="button"
-                                    className="bbutton RoundButton border btn btn-block btn-outline-success"
-                                    value="Other"
-                                    // onClick={() => this.changegender("Other")}
-                                >
-                                    Other
-                                </button>
+                        <div className="mt-3">
+                            <label className="text-color">Your gender</label>
+                            <div className="row mt-3">
+                                <div className="col-4">
+                                    <button
+                                        type="button"
+                                        className="bbutton col-12 border btn btn-block btn-success btn-outline-success"
+                                        value="Female"
+                                        // onClick={() => this.changegender("Female")}
+                                    >
+                                        Female
+                                    </button>
+                                </div>
+                                <div className="col-4">
+                                    <button
+                                        type="button"
+                                        className="bbutton border RoundButton btn btn-block btn-outline-success"
+                                        value="Male"
+                                        // onClick={() => this.changegender("Male")}
+                                    >
+                                        Male
+                                    </button>
+                                </div>
+                                <div className="col-4">
+                                    <button
+                                        type="button"
+                                        className="bbutton RoundButton border btn btn-block btn-outline-success"
+                                        value="Other"
+                                        // onClick={() => this.changegender("Other")}
+                                    >
+                                        Other
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="mt-5">
-                    <Button size="medium" fullwidth>
-                        Continue
-                    </Button>
-                </div>
-            </form>
-            {/* <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Your full name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        className="border p-2"
-                        placeholder="eg: Raj Kumar Babu"
-                    />
-                    <Form.Text className="text-muted">
-                        Ensure it matches name on your idendity documents
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" >
-                    <Form.Label>Your date of birth</Form.Label>
-                    <Form.Control type="date"  />
-                </Form.Group>
-                <Button variant="primary" >
-                    Submit
-                </Button>
-            </Form> */}
-        </div>
+                    <div className="mt-5">
+                        <Button
+                            onClick={firstStepHandler}
+                            size="medium"
+                            fullwidth
+                        >
+                            Continue
+                        </Button>
+                    </div>
+                </form>
+            </Modal.Body>
+        </>
     );
 }
 
