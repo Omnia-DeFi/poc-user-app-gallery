f521e14a60c8925760f28a02d1f235583e8d7653
Sahinur Islam <infosahinur@gmail.com>
2022-08-18 19:11:32 +0600

    [ADD] Select a Government ID

--- src/components/KYCModal/TakeGovernmentIDFront.jsx
+++ src/components/KYCModal/TakeGovernmentIDFront.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useState } from "react";
 // import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Button from "@ui/button";
 
 function TakeGovernmentIDFront({ fifthStepHandler }) {
+    const [driverLicense, setDriverLicense] = useState(true);
+    const [passport, setPassport] = useState(false);
+    const [nationalID, setNationalID] = useState(false);
+
+    const driverLicenseHandler = () => {
+        setDriverLicense(true);
+        setPassport(false);
+        setNationalID(false);
+    };
+    const passportHandler = () => {
+        setDriverLicense(false);
+        setPassport(true);
+        setNationalID(false);
+    };
+    const nationalIDHandler = () => {
+        setDriverLicense(false);
+        setPassport(false);
+        setNationalID(true);
+    };
+
     return (
         <>
             <Modal.Header>
@@ -28,13 +48,40 @@ function TakeGovernmentIDFront({ fifthStepHandler }) {
             <Modal.Body>
                 <div className="border border-2 p-5">
                     <h5 className="my-2">Select a Government ID</h5>
-                    <div></div>
-                    
+                    <div className="select-Government-ID">
+                        <div onClick={driverLicenseHandler}>
+                            Driving Licence
+                        </div>
+                        <div onClick={passportHandler}>Passport</div>
+                        <div onClick={nationalIDHandler}>National ID</div>
+                    </div>
+
                     <div className="mt-5">
-                        <div className="mb-5">
-                        <p>Front of Driving Licence</p>
-                        <p>Your name and photo should be clearly visible</p>
-                        <img src="/images/KYC/driving-licence-front.png" alt="selfie" />
+                        <div className="mb-5 modal-id-show">
+                            <p>
+                                Front of {driverLicense && "Driving Licence"}{" "}
+                                {passport && "Passport"}{" "}
+                                {nationalID && "National ID"}
+                            </p>
+                            <p>Your name and photo should be clearly visible</p>
+                            {driverLicense && (
+                                <img
+                                    src="/images/KYC/driving-licence-front.png"
+                                    alt="driving-licence-front"
+                                />
+                            )}
+                            {passport && (
+                                <img
+                                    src="/images/KYC/passport.png"
+                                    alt="passport-front"
+                                />
+                            )}
+                            {nationalID && (
+                                <img
+                                    src="/images/KYC/national-id-front.png"
+                                    alt="national-id-front"
+                                />
+                            )}
                         </div>
                         <Button
                             onClick={fifthStepHandler}
