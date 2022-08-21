ae99242e2139e20b626e20485fae4999e54f0cb1
siamparvez44 <siamparvez44@gmail.com>
2022-08-22 05:53:19 +0600

    feat: profile dropdown

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -9,14 +9,17 @@ import Anchor from "@ui/anchor";
 import Button from "@ui/button";
 import { useRouter } from "next/router";
 import { useOffcanvas, useSticky, useFlyoutSearch } from "@hooks";
-import { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useUserContext } from "src/context/context";
 import { logoutUser } from "src/context/actions";
 import headerData from "../../data/general/header.json";
 import menuData from "../../data/general/menu.json";
 import { magic } from "../../utils/magic";
 import Modal from "react-bootstrap/Modal";
+import Dropdown from "react-bootstrap/Dropdown";
 import IndexKYC from "@components/kyc-modal/IndexKYC";
+import Form from "react-bootstrap/Form";
+import DropdownMenu from "./DropdownMenu";
 
 const Header = ({ className }) => {
     const sticky = useSticky();
@@ -24,6 +27,7 @@ const Header = ({ className }) => {
     const { search, searchHandler } = useFlyoutSearch();
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const router = useRouter();
+    const [open, setOpen] = useState(false);
 
     const { state, dispatch } = useUserContext();
 
@@ -67,38 +71,81 @@ const Header = ({ className }) => {
                             </div>
                         </div>
                         <div className="header-right">
-                            {/* <div className="setting-option d-none d-lg-block">
-                                <SearchForm />
-                            </div>
-                            <div className="setting-option rn-icon-list d-block d-lg-none">
-                                <div className="icon-box search-mobile-icon">
-                                    <button
-                                        type="button"
-                                        aria-label="Click here to open search form"
-                                        onClick={searchHandler}
-                                    >
-                                        <i className="feather-search" />
-                                    </button>
+                            {isAuthenticated && (
+                               <>
+                                 <IndexKYC
+                                    show={showBidModal}
+                                    handleModal={handleBidModal}
+                                />
+                             
+                                <div className="setting-option rn-icon-list notification-badge">
+                                    <div className="icon-box">
+                                        <Anchor path={headerData.activity_link}>
+                                            <i className="feather-bell" />
+                                            <span className="badge">6</span>
+                                        </Anchor>
+                                    </div>
                                 </div>
-                                <FlyoutSearchForm isOpen={search} />
-                            </div> */}
-                            <IndexKYC
-                                show={showBidModal}
-                                handleModal={handleBidModal}
-                            />
-                            <div className="setting-option header-btn">
-                                <div className="icon-box">
-                                    <Button
-                                        color="primary-alta"
-                                        className="connectBtn"
-                                        size="small"
-                                        onClick={handleBidModal}
-                                    >
-                                        +
-                                    </Button>
-                                </div>
+                               </>
+                            )}
+                            <div
+                                id="my_switcher"
+                                className="setting-option my_switcher"
+                            >
+                                <ColorSwitcher />
                             </div>
-                            {!isAuthenticated && (
+                            
+                            <div
+                                onClick={offcanvasHandler}
+                                className="setting-option my_switcher mobile-menu-bar hamberger-menu-icon d-flex d-xl-none"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    width="21px"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            {isAuthenticated && (
+                                <div
+                                    id="dropdown-basic"
+                                    className="setting-option my_switcher user-menu-icon"
+                                    onClick={() => {
+                                        setOpen(!open);
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        width="21px"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </div>
+                            )}
+
+                            {open && (
+                                <DropdownMenu
+                                    state={state}
+                                    isAuthenticated={isAuthenticated}
+                                    logout={logout}
+                                    handleBidModal={handleBidModal}
+                                />
+                            )}
+                              {!isAuthenticated && (
                                 <div className="setting-option header-btn">
                                     <div className="icon-box">
                                         <Button
@@ -114,65 +161,6 @@ const Header = ({ className }) => {
                                     </div>
                                 </div>
                             )}
-                            {isAuthenticated && (
-                                <>
-                                    <div className="setting-option header-btn">
-                                        <div className="icon-box">
-                                            <Button
-                                                color="primary-alta"
-                                                className="connectBtn"
-                                                size="small"
-                                                onClick={logout}
-                                            >
-                                                Log Out
-                                            </Button>
-                                        </div>
-                                    </div>
-                                    <div className="setting-option header-btn">
-                                        <div className="icon-box">
-                                            <Button
-                                                color="primary-alta"
-                                                className="connectBtn"
-                                                size="small"
-                                            >
-                                                {state.issuer.slice(9, 15)}
-                                                ....
-                                                {state.issuer.slice(-4)}
-                                            </Button>
-                                        </div>
-                                    </div>
-                                    <div className="setting-option header-btn">
-                                        <div className="icon-box">
-                                            <Button
-                                                color="primary-alta"
-                                                className="connectBtn"
-                                                size="small"
-                                            >
-                                                {state.email}
-                                            </Button>
-                                        </div>
-                                    </div>
-                                </>
-                            )}
-                            <div className="setting-option rn-icon-list notification-badge">
-                                <div className="icon-box">
-                                    <Anchor path={headerData.activity_link}>
-                                        <i className="feather-bell" />
-                                        <span className="badge">6</span>
-                                    </Anchor>
-                                </div>
-                            </div>
-                            <div className="setting-option mobile-menu-bar d-block d-xl-none">
-                                <div className="hamberger">
-                                    <BurgerButton onClick={offcanvasHandler} />
-                                </div>
-                            </div>
-                            <div
-                                id="my_switcher"
-                                className="setting-option my_switcher"
-                            >
-                                <ColorSwitcher />
-                            </div>
                         </div>
                     </div>
                 </div>
